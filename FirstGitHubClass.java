b7f9bade243e31d7b3eebd5f70d13921f9e8e87d
wangbiqiang <30407518+wangbiqiang@users.noreply.github.com>
2020-11-30 00:03:57 +0800

    改变输出
    输出最后加上Again

--- FirstGitHubClass.java
+++ FirstGitHubClass.java
@@ -3,7 +3,7 @@ package cn.bdqn.test;
 public class FirstGitHubClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello , GitHub !");
+		System.out.println("Hello , GitHub ! Again ！！！");
 	}
 
 }
